b5ed9957791571426bba16cd736b8f3eed5878de
patrick.kleindienst <patrick.kleindienst@bertschinnovation.com>
2016-01-28 14:14:38 +0100

    Configured time-based rolling file logger and replaced print statements

--- elasticTreasure.py
+++ elasticTreasure.py
@@ -4,6 +4,8 @@ import json
 import requests
 import datetime
 import time
+import logging
+import logging.handlers
 
 class ElasticTreasure:
 
@@ -13,6 +15,10 @@ class ElasticTreasure:
 
 	SUCCESS_MESSAGE = "Elasticsearch backup successful"
 	FAILURE_MESSAGE = "Elasticsearch backup failed"
+
+	MILLIS_PER_DAY = 24 * 60 * 60 * 1000
+
+	
 
 
 
@@ -28,12 +34,20 @@ class ElasticTreasure:
 		self.__requestHeader = {'Content-Type':'application/json'}
 		self.__repoSettingsJSON = {"type":"fs", "settings": { "compress":"true", "location": self.__backupPath }}
 
+		log_handler = logging.handlers.TimedRotatingFileHandler(filename="/home/patrick.kleindienst/elasticTreasure/logs/treasure.log", when='W6', backupCount=5)
+		formatter = logging.Formatter('%(asctime)s - ElasticTreasure: %(message)s', '%b %d %H:%M:%S')
+		log_handler.setFormatter(formatter)
+		logger = logging.getLogger()
+		logger.addHandler(log_handler)
+		logger.setLevel(logging.INFO)
+
 
 
 ### 	Public API
 
 
 	def startBackupProcess(self):
+		logging.info("Starting backup process..")
 		response = self.__sendBackupRequest()
 		self.__evalResponse(response)		
 	
@@ -41,6 +55,7 @@ class ElasticTreasure:
 	
 
 	def cleanUpBackups(self):
+		logging.info("Starting clean up outdated backups..")
 		response = self.__retrieveAllSnapshots()
 		self.__deleteExpired(response)
 	
@@ -48,7 +63,6 @@ class ElasticTreasure:
 
 	def deleteAllSnapshotsFromRepo(self):
 		res = self.__retrieveAllSnapshots()
-		print "Statuscode: " + str(res.status_code)
 		snapshotList = json.loads(res.content)["snapshots"]
 		for s in snapshotList:
 			requests.delete(self.__elasticURI + "/_snapshot/" + self.__repositoryName + "/" + s["snapshot"])
@@ -56,8 +70,11 @@ class ElasticTreasure:
 
 
 	def restoreBackup(self, snapshotID):
+		logging.info("Start restoring snapshot: " + snapshotID)
 		if(True == self.__snapshotExists(snapshotID)):
 			self.__sendRestoringRequest(snapshotID)
+		else:
+			logging.error("Error: Snapshot " + snapshotID + " does not exist. Restoring process terminated.")
 
 
 
@@ -78,13 +95,13 @@ class ElasticTreasure:
         def __sendRestoringRequest(self, snapshotID):
                 response = requests.post(self.__elasticURI + "/_snapshot/" + self.__repositoryName + "/" + snapshotID + "/_restore")
                 if(self.HTTP_OK == response.status_code):
-                        print "Successfully restored backup: " + backupID
+                        logging.info("Successfully restored backup: " + backupID)
 		elif(self.HTTP_SERVER_ERROR == response.status_code):
-			print "Error: Are you trying to restore an open index? - " + response.content
+			logging.error("Error: Are you trying to restore an open index? - " + response.content)
                 elif(self.HTTP_NOT_FOUND == response.status_code):
-                        print "Error: Unable to find snapshot with name " + snapshotID + ". - " + response.content
+                        logging.error("Error: Unable to find snapshot with name " + snapshotID + ". - " + response.content)
 		else:
-			print "Oops, something went wrong!"
+			logging.error("Oops, something went wrong!")
 
 
 
@@ -94,6 +111,7 @@ class ElasticTreasure:
 	
 
 	def __retrieveAllSnapshots(self):
+		logging.info("Retrieve all snapshots from repository..")
 		response = requests.get(self.__elasticURI + "/_snapshot/" + self.__repositoryName + "/_all")
 		return response
 	
@@ -101,29 +119,36 @@ class ElasticTreasure:
 	
 	def __deleteExpired(self, response):
 		if(self.HTTP_OK == response.status_code):
+			logging.info("Start searching for expired backups..")
 			snapshotList = json.loads(response.content)["snapshots"]
 			for snapshot in snapshotList:
 				self.__removeIfExpired(snapshot)
 		else:
-			print "Nothing to backup!"
+			logging.info("Could not find any snapshots!")
 
 
 	def __removeIfExpired(self, snapshot):
 		snapshotAgeInMillis = snapshot["start_time_in_millis"]
+		snapshotName = snapshot["snapshot"]
 		currentTimeMillis = int(round(time.time() * 1000))
 		if((currentTimeMillis - snapshotAgeInMillis) > self.__backupExpirationInMillis()):
-			self.__sendDeleteRequest(snapshot["snapshot"])
+			self.__sendDeleteRequest(snapshotName)
+		else:
+			logging.info("Snapshot " + snapshotName + " is not expired yet.")
 
 
 	
 	def __sendDeleteRequest(self, snapshotName):
-		requests.delete(self.__elasticURI + "/_snapshot/" + self.__repositoryName + "/" + snapshotName) 
-
-
+		logging.info("Found outdated snapshot: " + snapshotName + " - Start sending delete request..")
+		response = requests.delete(self.__elasticURI + "/_snapshot/" + self.__repositoryName + "/" + snapshotName) 
+		if(self.HTTP_OK == response.status_code):
+			logging.info("Deleting snapshot " + snapshotName + " has been succesful.") 
+		else:
+			logging.info("Deleting snapshot " + snapshotName + " failed. Server says: " + response.content)
 	
 
 	def __backupExpirationInMillis(self):
-		expirationIntervalInMillis = self.__backupExpirationInDays * 24 * 60 * 1000
+		expirationIntervalInMillis = self.__backupExpirationInDays * self.MILLIS_PER_DAY 
 		return expirationIntervalInMillis	
 	
 
@@ -141,6 +166,7 @@ class ElasticTreasure:
 		if(repoStatus):
 			response = requests.put(self.__elasticURI + "/_snapshot/" + self.__repositoryName + "/backup_" + self.__getCurrentDateTime(),
 			headers=self.__requestHeader)
+			logging.info("Backup request has been sent. Server says: " + response.content)
 			return response
 		
 	
@@ -148,25 +174,26 @@ class ElasticTreasure:
 	def __isRepositoryReady(self):
 		response = requests.get(self.__elasticURI + "/_snapshot/" + self.__repositoryName)
 		if(self.HTTP_OK == response.status_code):
-			print "Repo is ready"
+			logging.info("Repository " + self.__repositoryName + " already exists.")
 			return True
 		else:
-			print "Repo is not ready. Creating repository.."
+			logging.info("Repository " + self.__repositoryName + " does not exist yet.")
 			if(self.__createRepoIfNotExists()):
 				return True
 			else:
-				print "Aborting."
+				logging.error("An error occurred. Backup procedure terminated.")
 				return False
 			 
 	
 
 	def __createRepoIfNotExists(self):
+		logging.info("Creating repository..")
 		response = requests.put(self.__elasticURI + "/_snapshot/" + self.__repositoryName, data=json.dumps(self.__repoSettingsJSON), headers=self.__requestHeader)
 		if(self.HTTP_OK == response.status_code):
-			print "Creation successful"
+			logging.info("Creating repository has been successful.")
 			return True
 		else:
-			print "Creation failed"
+			logging.error("Error: Creating repository failed!")
 			return False
 		
 
@@ -180,8 +207,10 @@ class ElasticTreasure:
 
 	def __evalResponse(self, response):
 		if(self.HTTP_OK == response.status_code and True == self.__sendMail):
+			logging.info("Request successful. Sending confirmation mail..")
 			self.__sendSuccessMail(response)
 		elif(True == self.__sendMail):
+			logging.info("Something went wrong. Start sending mail containing further details..")
 			self.__sendFailureMail(response)
 
 
